b237fc467f171a3ca769377758b66dfc71529eea
Joshua Peek <josh@joshpeek.com>
2014-11-26 11:29:06 -0600

    Test upload empty file

--- test/test.js
+++ test/test.js
@@ -282,6 +282,34 @@ function submit(form) {
       ok(window.request.headers['content-type'].match('multipart/form-data'), window.request.headers['content-type']);
     });
   });
+
+  promiseTest('form multipart POST request with empty file', 5, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'POST';
+      form.action = '/foo';
+      form.enctype = 'multipart/form-data';
+
+      var input = window.document.createElement('input');
+      input.type = 'file';
+      input.name = 'bar';
+      form.appendChild(input);
+
+      submit(form);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'POST');
+      equal(window.request.url, '/foo');
+      var lines = window.request.body.split(/\r\n?/);
+      equal(lines[1], 'Content-Disposition: form-data; name="bar"; filename=""');
+      equal(lines[3], '');
+      ok(window.request.headers['content-type'].match('multipart/form-data'), window.request.headers['content-type']);
+    });
+  });
 });
 
 promiseTest('form PUT request', 5, function() {
